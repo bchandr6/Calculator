10fae9d87f7b44cc56b8b009ab57bfe724c1bd76
Balaji Chandrasekaran <philanthropist09@gmail.com>
2016-02-22 02:32:49 -0700

    First Commit for Calculator

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,78 @@
+
+package cse360assign3;
+
+/**
+ * class to hold the methods and required variables for
+ * the purpose of implementing a calculator which adds,
+ * subtracts, multiplies and divides a number and even
+ * give you the history of calculation.
+ * 
+ *  @author Balaji Chandrasekaran 211 for CSE 360 Spring 2016
+ *	@version 1.0 Feb 22,2016
+ */
+
+public class Calculator {
+	
+	/**
+	 * creates a calculator object with all the class
+	 * variable initialized
+	 * 
+	 */	
+	public Calculator () {
+	}
+	
+	/**
+	 * This method returns the total of the calculation
+	 * done so far when called
+	 * @return int Result of the calculation done
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * This method is used for addition . It adds the given
+	 * value to the previously calculated value.
+	 * @param value the value that is to be added
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * This method is used for subtraction. It subtracts
+	 * the given value to the previously calculated value.
+	 * @param value the value value to be subtracted
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * This method is used for multiplication function
+	 * the given value is multiplied with the previous
+	 * result
+	 * @param value the value that is to be multiplied
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * This method is used for division function
+	 * the given value is used as a divisor.
+	 * @param value the value used as a divisor
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * This method returns the history of the calculation
+	 * in a string format.
+	 * @return String this string contains history of operations
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
